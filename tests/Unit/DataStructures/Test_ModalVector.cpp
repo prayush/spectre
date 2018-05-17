25d10265961bb46f22daaa36e67fa4c0592c9411
Prayush Kumar <prayush.kumar@ligo.org>
2018-05-17 20:02:43 +0530

    dummy commit 002 to find the heisenbug

--- tests/Unit/DataStructures/Test_ModalVector.cpp
+++ tests/Unit/DataStructures/Test_ModalVector.cpp
@@ -436,10 +436,10 @@ SPECTRE_TEST_CASE("Unit.DataStructures.ModalVector.MathWithDataVector",
   ModalVector nine(num_pts, 9.0);
   DataVector eight(num_pts, 8.0);
 
-  //~ // Test math with DataVector
-  //~ ModalVector test_72(num_pts, -1.0);
-  //~ test_72 = nine * eight;
-  //~ check_vectors(ModalVector(num_pts, 72.0), test_72);
+  // Test math with DataVector
+  ModalVector test_72(num_pts, -1.0);
+  test_72 = nine * eight;
+  check_vectors(ModalVector(num_pts, 72.0), test_72);
   //~ CHECK(test_72.is_owning());
   //~ test_72 = eight * nine;
   //~ check_vectors(ModalVector(num_pts, 72.0), test_72);
