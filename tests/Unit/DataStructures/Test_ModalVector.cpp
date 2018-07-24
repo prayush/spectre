32ec239b6d74a7806fbaec207c1e85c08ab65e22
Prayush Kumar <prayush.kumar@ligo.org>
2018-07-24 16:39:02 -0400

    remove debugging code

--- tests/Unit/DataStructures/Test_ModalVector.cpp
+++ tests/Unit/DataStructures/Test_ModalVector.cpp
@@ -9,16 +9,16 @@
 #include <cstddef>
 #include <functional>  // for std::reference_wrapper
 #include <numeric>
-#include <iostream>
-#include <typeinfo>
-#include <type_traits>
-#include <typeinfo>
-#ifndef _MSC_VER
-#   include <cxxabi.h>
-#endif
-#include <memory>
-#include <string>
-#include <cstdlib>
+//#include <iostream>
+//#include <typeinfo>
+//#include <type_traits>
+//#include <typeinfo>
+//~ #ifndef _MSC_VER
+//~ #   include <cxxabi.h>
+//~ #endif
+//~ #include <memory>
+//~ #include <string>
+//~ #include <cstdlib>
 
 #include "DataStructures/ModalVector.hpp"
 #include "DataStructures/DataVector.hpp"
@@ -31,30 +31,30 @@
 #include "Utilities/StdHelpers.hpp"  // IWYU pragma: keep
 #include "tests/Unit/TestHelpers.hpp"
 
-template <class T> std::string type_name()
-{
-    typedef typename std::remove_reference<T>::type TR;
-    std::unique_ptr<char, void(*)(void*)> own
-           (
-#ifndef _MSC_VER
-                abi::__cxa_demangle(typeid(TR).name(), nullptr,
-                                           nullptr, nullptr),
-#else
-                nullptr,
-#endif
-                std::free
-           );
-    std::string r = own != nullptr ? own.get() : typeid(TR).name();
-    if (std::is_const<TR>::value)
-        r += " const";
-    if (std::is_volatile<TR>::value)
-        r += " volatile";
-    if (std::is_lvalue_reference<T>::value)
-        r += "&";
-    else if (std::is_rvalue_reference<T>::value)
-        r += "&&";
-    return r;
-}
+//~ template <class T> std::string type_name()
+//~ {
+    //~ typedef typename std::remove_reference<T>::type TR;
+    //~ std::unique_ptr<char, void(*)(void*)> own
+           //~ (
+//~ #ifndef _MSC_VER
+                //~ abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                           //~ nullptr, nullptr),
+//~ #else
+                //~ nullptr,
+//~ #endif
+                //~ std::free
+           //~ );
+    //~ std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    //~ if (std::is_const<TR>::value)
+        //~ r += " const";
+    //~ if (std::is_volatile<TR>::value)
+        //~ r += " volatile";
+    //~ if (std::is_lvalue_reference<T>::value)
+        //~ r += "&";
+    //~ else if (std::is_rvalue_reference<T>::value)
+        //~ r += "&&";
+    //~ return r;
+//~ }
 
 SPECTRE_TEST_CASE("Unit.DataStructures.ModalVector",
                   "[DataStructures][Unit]") {
