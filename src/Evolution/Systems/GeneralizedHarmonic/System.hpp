b65204011f6a9ef1bc865c6cc0dc8183e7829b51
Prayush Kumar <prayush.kumar@ligo.org>
2019-04-25 17:07:28 -0400

    Update GH System with tags needed by its exe
    Signed-off-by: Prayush Kumar <prayush.kumar@ligo.org>

--- src/Evolution/Systems/GeneralizedHarmonic/System.hpp
+++ src/Evolution/Systems/GeneralizedHarmonic/System.hpp
@@ -3,17 +3,27 @@
 
 #pragma once
 
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Characteristics.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/Equations.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/TagsDeclarations.hpp"
 #include "PointwiseFunctions/GeneralRelativity/TagsDeclarations.hpp"
+#include "Utilities/TMPL.hpp"
 
 /// \cond
+class DataVector;
+
 namespace brigand {
 template <class...>
 struct list;
 }  // namespace brigand
 
+namespace Tags {
 template <class>
 class Variables;
+}  // namespace Tags
 /// \endcond
 
 /*!
@@ -28,10 +38,27 @@ struct System {
   static constexpr size_t volume_dim = Dim;
   static constexpr bool is_euclidean = false;
 
-  using variables_tags = brigand::list<gr::Tags::SpacetimeMetric<Dim>,
-                                       Tags::Pi<Dim>, Tags::Phi<Dim>>;
-  using gradient_tags = variables_tags;
+  using variables_tag = ::Tags::Variables<tmpl::list<
+      gr::Tags::SpacetimeMetric<Dim, Frame::Inertial, DataVector>,
+      Tags::Pi<Dim, Frame::Inertial>, Tags::Phi<Dim, Frame::Inertial>>>;
+  using gradients_tags =
+      tmpl::list<gr::Tags::SpacetimeMetric<Dim, Frame::Inertial, DataVector>,
+                 Tags::Pi<Dim, Frame::Inertial>,
+                 Tags::Phi<Dim, Frame::Inertial>>;
+  // `extras_tag` can be used with `Tags::DerivCompute` to get spatial
+  // derivatives of quantities that are otherwise not available within
+  // a `Variables<>` container.
+  using extras_tag = ::Tags::Variables<
+      tmpl::list<GeneralizedHarmonic::Tags::GaugeH<Dim, Frame::Inertial>>>;
 
-  using Variables = ::Variables<variables_tags>;
+  using compute_time_derivative = ComputeDuDt<Dim>;
+  using normal_dot_fluxes = ComputeNormalDotFluxes<Dim>;
+  using char_speeds_tag = CharacteristicSpeedsCompute<Dim, Frame::Inertial>;
+  using compute_largest_characteristic_speed =
+      ComputeLargestCharacteristicSpeed<Dim, Frame::Inertial>;
+
+  template <typename Tag>
+  using magnitude_tag = ::Tags::NonEuclideanMagnitude<
+      Tag, gr::Tags::InverseSpatialMetric<Dim, Frame::Inertial, DataVector>>;
 };
 }  // namespace GeneralizedHarmonic
