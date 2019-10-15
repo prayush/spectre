682232e8bbb0331ac05bdee6594fb8b847617c67
Prayush Kumar <prayush@astro.cornell.edu>
2019-10-14 21:13:17 -0700

    modify GenerateXdmf to create Xdmfs for 2D data

--- tools/GenerateXdmf.py
+++ tools/GenerateXdmf.py
@@ -49,44 +49,45 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
             h5temporal = h5file[0].get('element_data.vol').get(id_and_value[0])
             extents = h5temporal.get("total_extents")
             extents_x = np.array([extents[i] for i in
-                                  range (extents.size) if i%3 == 0])
-            extents_y = np.array( [extents[i] for i in
-                                   range (extents.size) if i%3 == 1])
-            extents_z = np.array( [extents[i] for i in
-                                   range (extents.size) if i%3 == 2])
+                                  range(extents.size) if i % 3 == 0])
+            extents_y = np.array([extents[i] for i in
+                                  range(extents.size) if i % 3 == 1])
+            extents_z = np.array([extents[i] for i in
+                                  range(extents.size) if i % 3 == 2])
 
             total_extents_x = sum(extents_x)
             total_extents_y = sum(extents_y)
             total_extents_z = sum(extents_z)
-            numpoints = sum(extents_x*extents_y*extents_z)
-            number_of_cells = sum((extents_x-1)*(extents_y-1)*(extents_z-1))
+            numpoints = sum(extents_x * extents_y * extents_z)
+            number_of_cells = sum(
+                (extents_x - 1) * (extents_y - 1) * (extents_z - 1))
             data_item = "        <DataItem Dimensions=\" %d\" " \
-                    "NumberType=\"Double\" Precision=\"8\" Format=\"HDF5\">\n" \
-                    % (numpoints)
+                "NumberType=\"Double\" Precision=\"8\" Format=\"HDF5\">\n" \
+                % (numpoints)
             data_item_vec = "        <DataItem Dimensions=\" %d 3\" "\
-                    "ItemType = \"Function\" Function = \"JOIN($0,$1,$2)\">\n"\
+                "ItemType = \"Function\" Function = \"JOIN($0,$1,$2)\">\n"\
                             % (numpoints)
             Grid_path = "          %s:/element_data.vol/%s" % (
                 h5file[1], id_and_value[0])
             xdmf_output += \
-                           "    <Grid Name=\"%s\" GridType=\"Uniform\">\n" \
-                           % (h5file[1])
+                "    <Grid Name=\"%s\" GridType=\"Uniform\">\n" \
+                % (h5file[1])
             # Write topology information
             xdmf_output += "      <Topology TopologyType=\"Hexahedron\" " \
                            "NumberOfElements=\"%d\">\n" % (number_of_cells)
             xdmf_output += "        <DataItem Dimensions=\"%d 8\" " \
                            "NumberType=\"Int\" Format=\"HDF5\">\n" % (
                                number_of_cells)
-            xdmf_output += Grid_path  + "/connectivity\n" \
-                           "        </DataItem>\n      </Topology>\n"
+            xdmf_output += Grid_path + "/connectivity\n" \
+                "        </DataItem>\n      </Topology>\n"
             # Write geometry/coordinates
             xdmf_output += "      <Geometry Type=\"X_Y_Z\">\n"
             xdmf_output += data_item + Grid_path + \
-                            "/InertialCoordinates_x\n        </DataItem>\n"
+                "/InertialCoordinates_x\n        </DataItem>\n"
             xdmf_output += data_item + Grid_path + \
-                           "/InertialCoordinates_y\n        </DataItem>\n"
+                "/InertialCoordinates_y\n        </DataItem>\n"
             xdmf_output += data_item + Grid_path + \
-                           "/InertialCoordinates_z\n        </DataItem>\n"
+                "/InertialCoordinates_z\n        </DataItem>\n"
             xdmf_output += "      </Geometry>\n"
             # Everything that isn't a coordinate is a "component"
             components = list(h5temporal.keys())
@@ -106,12 +107,12 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
                             vector)
                     xdmf_output += data_item_vec
                     for index in ["_x", "_y", "_z"]:
-                        xdmf_output += data_item + Grid_path  + \
-                                       "/%s" %(vector) + index + "\n"   + \
-                                       "        </DataItem>\n"
+                        xdmf_output += data_item + Grid_path + \
+                            "/%s" % (vector) + index + "\n" + \
+                            "        </DataItem>\n"
                     xdmf_output += "        </DataItem>\n"
                     xdmf_output += "      </Attribute>\n"
-                elif(component.endswith("_y") or  \
+                elif(component.endswith("_y") or
                      component.endswith("_z")):
                     # The component is a y or z component of a vector
                     # it will be processed with the x component
@@ -121,9 +122,131 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
                     # write it as a scalar
                     xdmf_output += "      <Attribute Name=\"%s\" " \
                         "AttributeType=\"Scalar\" Center=\"Node\">\n" % (
-                        component)
+                            component)
                     xdmf_output += data_item + Grid_path + (
                         "/%s\n" % component) + "        </DataItem>\n"
+                    xdmf_output += "      </Attribute>\n"
+            xdmf_output += "    </Grid>\n"
+
+        # close time grid
+        xdmf_output += "  </Grid>\n"
+
+    xdmf_output += "</Grid>\n</Domain>\n</Xdmf>"
+
+    for h5file in h5files:
+        h5file[0].close()
+
+    with open(output_filename + ".xmf", "w") as xmf_file:
+        xmf_file.write(xdmf_output)
+
+
+def generate_xdmf_2d(file_prefix, output_filename,
+                     start_time, stop_time, stride):
+    """
+    Generate one XDMF file that ParaView and VisIt can use to load the data
+    out of the HDF5 files.
+    """
+    h5files = [(h5py.File(filename, 'r'), filename)
+               for filename in glob.glob(file_prefix + "*.h5")]
+
+    element_data = h5files[0][0].get('element_data.vol')
+    temporal_ids_and_values = [(x,
+                                element_data.get(x).attrs['observation_value'])
+                               for x in element_data.keys()]
+    temporal_ids_and_values.sort(key=lambda x: x[1])
+
+    xdmf_output = "<?xml version=\"1.0\" ?>\n" \
+        "<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\">\n" \
+        "<Xdmf Version=\"2.0\">\n" \
+        "<Domain>\n" \
+        "<Grid Name=\"Evolution\" GridType=\"Collection\" " \
+        "CollectionType=\"Temporal\">\n"
+
+    # counter used to enforce the stride
+    stride_counter = 0
+    for id_and_value in temporal_ids_and_values:
+        if id_and_value[1] < start_time:
+            continue
+        if id_and_value[1] > stop_time:
+            break
+
+        stride_counter += 1
+        if (stride_counter - 1) % stride != 0:
+            continue
+
+        h5temporal = element_data.get(id_and_value[0])
+        xdmf_output += "  <Grid Name=\"Grids\" GridType=\"Collection\">\n"
+        xdmf_output += "    <Time Value=\"%1.14e\"/>\n" % (id_and_value[1])
+        # loop over each h5 file
+        for h5file in h5files:
+            h5temporal = h5file[0].get('element_data.vol').get(id_and_value[0])
+            extents = h5temporal.get("total_extents")
+            extents_x = np.array([extents[i] for i in
+                                  range(extents.size) if i % 2 == 0])
+            extents_y = np.array([extents[i] for i in
+                                  range(extents.size) if i % 2 == 1])
+
+            total_extents_x = sum(extents_x)
+            total_extents_y = sum(extents_y)
+            numpoints = sum(extents_x * extents_y)
+            number_of_cells = sum((extents_x - 1) * (extents_y - 1))
+            data_item = "        <DataItem Dimensions=\" %d\" " \
+                "NumberType=\"Double\" Precision=\"8\" Format=\"HDF5\">\n" % (
+                    numpoints)
+            data_item_vec = "        <DataItem Dimensions=\" %d 2\" " \
+                "ItemType = \"Function\" Function = \"JOIN($0,$1)\">\n" % (
+                    numpoints)
+            Grid_path = "          %s:/element_data.vol/%s" % (
+                h5file[1], id_and_value[0])
+            xdmf_output += "    <Grid Name=\"%s\" GridType=\"Uniform\">\n" % (
+                h5file[1])
+            # Write topology information
+            xdmf_output += "      <Topology TopologyType=\"Quadrilateral\" " \
+                "NumberOfElements=\"%d\">\n" % (number_of_cells)
+            xdmf_output += "        <DataItem Dimensions=\"%d 4\"  " \
+                "NumberType=\"Int\" Format=\"HDF5\">\n" % (number_of_cells)
+            xdmf_output += Grid_path + \
+                "/connectivity\n        </DataItem>\n      </Topology>\n"
+            # Write geometry/coordinates
+            xdmf_output += "      <Geometry Type=\"X_Y\">\n"
+            xdmf_output += data_item + Grid_path + \
+                "/InertialCoordinates_x\n        </DataItem>\n"
+            xdmf_output += data_item + Grid_path + \
+                "/InertialCoordinates_y\n        </DataItem>\n"
+            xdmf_output += "      </Geometry>\n"
+            # Everything that isn't a coordinate is a "component"
+            components = list(h5temporal.keys())
+            components.remove('InertialCoordinates_x')
+            components.remove('InertialCoordinates_y')
+            components.remove('connectivity')
+            components.remove('total_extents')
+            components.remove('grid_names')
+            for component in components:
+                if component.endswith("_x"):
+                    # Write a vector using the three components that make up
+                    # the vector (i.e. v_x, v_y)
+                    vector = component[:-1]
+                    xdmf_output += "      <Attribute Name=\"%s\"  " \
+                        "AttributeType=\"Vector\" Center=\"Node\">\n" % (vector)
+                    xdmf_output += data_item_vec
+                    for index in ["_x", "_y"]:
+                        xdmf_output += data_item + Grid_path + \
+                            "/%s" % (vector) + index + "\n" + \
+                            "        </DataItem>\n"
+                    xdmf_output += "        </DataItem>\n"
+                    xdmf_output += "      </Attribute>\n"
+                elif(component.endswith("_y")):
+                    # The component is a y component of a vector
+                    # it will be processed with the x component
+                    continue
+                else:
+                    # If the component is not part of a vector,
+                    # write it as a scalar
+                    xdmf_output += "      <Attribute Name=\"%s\"  " \
+                        "AttributeType=\"Scalar\" Center=\"Node\">\n" % (
+                            component)
+                    xdmf_output += data_item + Grid_path + \
+                        ("/%s\n" % component) + "        </DataItem>\n"
                     xdmf_output += "      </Attribute>\n"
             xdmf_output += "    </Grid>\n"
 
@@ -174,11 +297,20 @@ def parse_args():
         type=float,
         help="The time at which to stop visualizing. The stop-time value is "
         "not included.")
+    parser.add_argument(
+        "--two-dimensional",
+        action='store_true',
+        help="Operate on volume data in 2 spatial dimensions.")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     input_args = parse_args()
-    generate_xdmf(input_args.file_prefix, input_args.output,
-                  input_args.start_time, input_args.stop_time,
-                  input_args.stride)
+    if not input_args.two_dimensional:
+        generate_xdmf(input_args.file_prefix, input_args.output,
+                      input_args.start_time, input_args.stop_time,
+                      input_args.stride)
+    else:
+        generate_xdmf_2d(input_args.file_prefix, input_args.output,
+                         input_args.start_time, input_args.stop_time,
+                         input_args.stride)
