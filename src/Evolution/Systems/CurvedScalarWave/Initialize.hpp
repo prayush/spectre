0779629e3a6fa84f9c892026bc595fa91fd6d072
Prayush Kumar <prayush@astro.cornell.edu>
2019-12-11 00:14:00 -0800

    Add preliminary executable for CurvedScalarWave

---
+++ src/Evolution/Systems/CurvedScalarWave/Initialize.hpp
@@ -0,0 +1,104 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <tuple>
+#include <utility>  // IWYU pragma: keep
+#include <vector>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/DataBoxTag.hpp"
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
+#include "DataStructures/Tensor/EagerMath/Norms.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/Mesh.hpp"
+#include "Domain/Tags.hpp"
+#include "ErrorHandling/Assert.hpp"
+#include "Evolution/Initialization/DiscontinuousGalerkin.hpp"
+#include "Evolution/Initialization/Evolution.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Characteristics.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Constraints.hpp"
+#include "Evolution/Systems/CurvedScalarWave/System.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
+#include "Parallel/ConstGlobalCache.hpp"
+#include "ParallelAlgorithms/Initialization/MergeIntoDataBox.hpp"
+#include "PointwiseFunctions/AnalyticData/Tags.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Christoffel.hpp"
+#include "PointwiseFunctions/GeneralRelativity/ComputeGhQuantities.hpp"
+#include "PointwiseFunctions/GeneralRelativity/ComputeSpacetimeQuantities.hpp"
+#include "PointwiseFunctions/GeneralRelativity/IndexManipulation.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TypeTraits.hpp"
+
+namespace CurvedScalarWave {
+namespace Actions {
+template <size_t Dim>
+struct InitializeGrVars {
+  using frame = Frame::Inertial;
+
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static auto apply(db::DataBox<DbTagsList>& box,
+                    const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using compute_tags = db::AddComputeTags<
+        Tags::ConstraintGamma1Compute, Tags::ConstraintGamma2Compute,
+        gr::Tags::SpatialChristoffelFirstKindCompute<Dim, Frame::Inertial,
+                                                     DataVector>,
+        gr::Tags::SpatialChristoffelSecondKindCompute<Dim, Frame::Inertial,
+                                                      DataVector>,
+        gr::Tags::TraceSpatialChristoffelSecondKindCompute<Dim, Frame::Inertial,
+                                                           DataVector>,
+        GeneralizedHarmonic::Tags::TraceExtrinsicCurvatureCompute<
+            Dim, Frame::Inertial>>;
+
+    return std::make_tuple(
+        Initialization::merge_into_databox<InitializeGrVars,
+                                           db::AddSimpleTags<>, compute_tags>(
+            std::move(box)));
+  }
+};
+
+template <size_t Dim>
+struct InitializeConstraints {
+  using frame = Frame::Inertial;
+
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static auto apply(db::DataBox<DbTagsList>& box,
+                    const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using compute_tags = db::AddComputeTags<
+        // CharacteristicSpeedsCompute<Dim>, CharacteristicFieldsCompute<Dim>,
+        Tags::OneIndexConstraintCompute<Dim>,
+        Tags::TwoIndexConstraintCompute<Dim>,
+        // following tags added to observe constraints
+        ::Tags::PointwiseL2NormCompute<Tags::OneIndexConstraint<Dim>>,
+        ::Tags::PointwiseL2NormCompute<Tags::TwoIndexConstraint<Dim>>>;
+
+    return std::make_tuple(
+        Initialization::merge_into_databox<InitializeConstraints,
+                                           db::AddSimpleTags<>, compute_tags>(
+            std::move(box)));
+  }
+};
+
+}  // namespace Actions
+}  // namespace CurvedScalarWave
--- src/Evolution/Systems/CurvedScalarWave/System.hpp
+++ src/Evolution/Systems/CurvedScalarWave/System.hpp
@@ -5,14 +5,18 @@
 
 #include <cstddef>
 
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Characteristics.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Equations.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
 #include "Utilities/TMPL.hpp"
 
+/// \cond
 namespace Tags {
 template <class>
 class Variables;
 }  // Namespace Tags
+/// \endcond
 
 /*!
  * \ingroup EvolutionSystemsGroup
@@ -25,10 +29,35 @@ struct System {
   static constexpr bool is_in_flux_conservative_form = false;
   static constexpr bool has_primitive_and_conservative_vars = false;
   static constexpr size_t volume_dim = Dim;
+  static constexpr bool is_euclidean = false;
 
   using variables_tag = ::Tags::Variables<tmpl::list<Pi, Phi<Dim>, Psi>>;
   using gradients_tags = tmpl::list<Pi, Phi<Dim>, Psi>;
+  using spacetime_variables_tag = ::Tags::Variables<tmpl::list<
+      gr::Tags::Lapse<DataVector>, ::Tags::dt<gr::Tags::Lapse<DataVector>>,
+      ::Tags::deriv<gr::Tags::Lapse<DataVector>, tmpl::size_t<Dim>,
+                    Frame::Inertial>,
+      gr::Tags::Shift<volume_dim, Frame::Inertial, DataVector>,
+      ::Tags::dt<gr::Tags::Shift<volume_dim, Frame::Inertial, DataVector>>,
+      ::Tags::deriv<gr::Tags::Shift<Dim, Frame::Inertial, DataVector>,
+                    tmpl::size_t<Dim>, Frame::Inertial>,
+      gr::Tags::SpatialMetric<volume_dim, Frame::Inertial, DataVector>,
+      ::Tags::dt<
+          gr::Tags::SpatialMetric<volume_dim, Frame::Inertial, DataVector>>,
+      ::Tags::deriv<gr::Tags::SpatialMetric<Dim, Frame::Inertial, DataVector>,
+                    tmpl::size_t<Dim>, Frame::Inertial>,
+      gr::Tags::SqrtDetSpatialMetric<DataVector>,
+      gr::Tags::ExtrinsicCurvature<volume_dim, Frame::Inertial, DataVector>,
+      gr::Tags::InverseSpatialMetric<volume_dim, Frame::Inertial, DataVector>>>;
 
   using compute_time_derivative = ComputeDuDt<Dim>;
+  using normal_dot_fluxes = ComputeNormalDotFluxes<Dim>;
+  using char_speeds_tag = CharacteristicSpeedsCompute<Dim>;
+  using compute_largest_characteristic_speed =
+      ComputeLargestCharacteristicSpeed<Dim>;
+
+  template <typename Tag>
+  using magnitude_tag = ::Tags::NonEuclideanMagnitude<
+      Tag, gr::Tags::InverseSpatialMetric<Dim, Frame::Inertial, DataVector>>;
 };
 }  // namespace CurvedScalarWave
