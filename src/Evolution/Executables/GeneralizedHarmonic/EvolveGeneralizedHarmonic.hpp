180995eb56b8c558d614a6890c451824d9ce3c92
Prayush Kumar <prayush@astro.cornell.edu>
2020-09-03 11:55:05 -0700

    fixup:
    Revert "Add an exponential filter to EvolveGeneralizedHarmonic" as
    it should be separate from this PR.
    
    This reverts commit 4229feb0d76fc2aeb95d1db8e87c59dfa1892420

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
@@ -64,8 +64,6 @@
 #include "NumericalAlgorithms/Interpolation/InterpolatorRegisterElement.hpp"
 #include "NumericalAlgorithms/Interpolation/Tags.hpp"
 #include "NumericalAlgorithms/Interpolation/TryToInterpolate.hpp"
-#include "NumericalAlgorithms/LinearOperators/ExponentialFilter.hpp"
-#include "NumericalAlgorithms/LinearOperators/FilterAction.hpp"  // IWYU pragma: keep
 #include "Options/Options.hpp"
 #include "Parallel/Actions/TerminatePhase.hpp"
 #include "Parallel/InitializationFunctions.hpp"
@@ -152,10 +150,6 @@ struct EvolutionMetavars {
 
   using normal_dot_numerical_flux = Tags::NumericalFlux<
       GeneralizedHarmonic::UpwindPenaltyCorrection<volume_dim>>;
-  // A filter is added here, but when performing numerical experiments with the
-  // generalized harmonic system, the user should determine whether this filter
-  // can be removed.
-  static constexpr bool use_filtering = true;
 
   using step_choosers_common =
       tmpl::list<StepChoosers::Registrars::Cfl<volume_dim, Frame::Inertial>,
@@ -338,15 +332,7 @@ struct EvolutionMetavars {
                                             EvolutionMetavars>>,
                          tmpl::list<>>,
                      Actions::RecordTimeStepperData<>>>,
-      Actions::UpdateU<>,
-      tmpl::conditional_t<
-          use_filtering,
-          dg::Actions::Filter<
-              Filters::Exponential<0>,
-              tmpl::list<gr::Tags::SpacetimeMetric<volume_dim, frame>,
-                         GeneralizedHarmonic::Tags::Pi<volume_dim, frame>,
-                         GeneralizedHarmonic::Tags::Phi<volume_dim, frame>>>,
-          tmpl::list<>>>;
+      Actions::UpdateU<>>;
 
   enum class Phase {
     Initialization,
